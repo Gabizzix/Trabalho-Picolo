55904bb8740a754cce8dcfce77a34a27d1cd4673
Gabizzix <gabriela2mattos5@gmail.com>
2023-06-20 18:04:03 -0400

    Trabalho - Picolo

--- script.js
+++ script.js
@@ -30,17 +30,22 @@ class Produto {
             </br>
             `
         } else {
-            throw new deuErro("Está faltando alguma informação. ;)")
+          throw new deuErro("Estão faltando informações.)")
         } 
     }
 }
 
 class ProdutoDestaque extends Produto {
-    constructor(nome, data_de_cadastro, descricao, preco, imagemDestaque){
-        super(nome, data_de_cadastro, descricao, preco)
-        this.imagemDestaque = imagemDestaque
+constructor(nome, data_de_cadastro, descricao, imgDestaque){
+        super(nome, data_de_cadastro, descricao,)
+        this.imgDestaque = imgDestaque
     }
-    mostrarProdutoDestaque(){
+
+
+//-- mostrar produto em destaque --//
+
+
+mostrarProdutoDestaque(){
         try {
             return this.montarProdutoDestaque()
         } catch(erro){
@@ -48,49 +53,73 @@ class ProdutoDestaque extends Produto {
         }
     }
 
-    montarProdutoDestaque(){
-        if(this.nome != "" && this.data_de_cadastro != "" && this.descricao != "" && this.preco != "" && this.imagemDestaque != ""){
+
+//-- montar produto em destaque --//
+
+
+montarProdutoDestaque(){
+if(this.nome != "" && this.data_de_cadastro != "" && this.descricao != "" && this.imgDestaque != ""){
             return `
-            <img src="${this.imagemDestaque}" width="593"></img>
+            <img src="${this.imgDestaque}" width="593"></img>
             <h4 id="nomeDest">${this.nome}</h4>
             <p id="p2">${this.descricao}
             </br>
-            <p id="p3">R$${this.preco},00</p>
             <p id="p4">${this.data_de_cadastro}</p>
             </br>
             `
         } else{
-            throw new deuErro("Está faltando alguma informação. ;)")
+            throw new deuErro("Estão faltando informações.)")
         }
     }
 }
 
-const produto = new Produto("PANDORA", 250806, "<h4><i> Anel Chevron Brilhante</i></h4> Em até 10x R$ 57,90 sem juros", 579,00, "anél1.jpg");
+
+
+//-- produto destaque --//
+
+
+const produtoDestaque = new ProdutoDestaque("<h1>PANDORA</h1>", 121022, "<h3> RECEBA NOSSAS NOVIDADES</h3> <h4><i> Os anéis da PANDORA são trabalhados com diferentes tons de Ouro 14K e Prata de Lei e apresentam pedras preciosas, pedras ou pérolas cultivadas. Os anéis da PANDORA podem ser utilizados sozinhos ou combinados, criando joias únicas! </i></h4>","anél1.jpg");
+console.log(produtoDestaque);
+
+
+
+//-- produtos --//
+
+
+const produto = new Produto("PANDORA", 250806, "<h4><i>Anel Chevron Brilhante</i></h4>           <h4>Simples e elegante, este anel em Y pode ser usado sozinho, como uma peça de destaque brilhante, ou combinado com outros anéis.</h4>", 579);
 console.log(produto);
 
-const produto2 = new Produto("PANDORA", 130406, "<h4><i> Anel Chevron Brilhante</i></h4> Em até 10x R$ 57,90 sem juros", 30, "anél1.jpg");
+
+const produto1 = new Produto("PANDORA", 250806, "<h4><i>Anel Riqueza Botanica Brilhante</i></h4> <h4>Nosso Anel Riqueza Botânica Brilhante ostenta três pedras centrais em formato de pétala, duas em lapidação navete e uma em gota.</h4>", 1.129);
+console.log(produto1);
+
+
+const produto2 = new Produto("PANDORA", 130203, "<h4><i>Anel Rosetm Beleza Clássica</i></h4>     <h4>Adicione um toque sutil de brilho a cada roupa com este anel, simples e elegante.</h4><br>", 1.259);
 console.log(produto2);
 
-const produto3 = new Produto("PANDORA", 121206, "<h4><i> Anel Chevron Brilhante</i></h4> Em até 10x R$ 57,90 sem juros", 90, "anél1.jpg");
+
+const produto3 = new Produto("PANDORA", 130203, "<h4><i>Anel Coração Vermelho Elevado</i></h4>   <h4>Expresse o verdadeiro desejo do seu coração com este anel Sparkle & Hearts. Finalizado à mão em banho de ouro rosa 14k.</h4>", 1.059);
 console.log(produto3);
 
-const produto4 = new Produto("PANDORA", 121206, "<h4><i> Anel Chevron Brilhante</i></h4> Em até 10x R$ 57,90 sem juros", 90, "anél1.jpg");
-console.log(produto4);
 
-const produtoDestaque = new ProdutoDestaque("PANDORA", 121022, "<h4> RECEBA NOSSAS NOVIDADES <br></br><i> Compre suas Joias Pandora parcelando em até 10x no cartão. Diversos modelos disponíveis. Os anéis da PANDORA podem ser utilizados sozinhos ou combinados, criando joias únicas! </i></h4>", 42, "anél1.jpg");
-console.log(produtoDestaque);
+
+//-- var destaque --//
 
 var destaque = document.getElementById("produto-destaque");
 destaque.insertAdjacentHTML('afterbegin', produtoDestaque.mostrarProdutoDestaque());
 
+
 var prod = document.getElementById("produto");
 prod.insertAdjacentHTML('afterbegin', produto.mostrarProduto());
+
+
+var prod = document.getElementById("produto1");
+prod.insertAdjacentHTML('afterbegin', produto1.mostrarProduto());
+
 
 var prod = document.getElementById("produto2");
 prod.insertAdjacentHTML('afterbegin', produto2.mostrarProduto());
 
+
 var prod = document.getElementById("produto3");
 prod.insertAdjacentHTML('afterbegin', produto3.mostrarProduto());
-
-var prod = document.getElementById("produto4");
-prod.insertAdjacentHTML('afterbegin', produto4.mostrarProduto());
